69655de1877c253e7e4981f9a92f8869f62010cc
Allan Perry <alperry21@gmail.com>
2024-08-18 09:00:09 -0600

    Sprint 11 complete

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,18 @@
-// Import the Spinner component into this file and test
-// that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+// // Import the Spinner component into this file and test
+// // that it renders what it should for the different props it can take.
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
+
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Spinner from './Spinner';
+
+describe('Spinner Component', () => {
+  it('should render when "on" is true', () => {
+    render(<Spinner on={true} />);
+    const spinnerElement = screen.getByTestId('spinner');
+    expect(spinnerElement).toBeInTheDocument();
+  });
+});
